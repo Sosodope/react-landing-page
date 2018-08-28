9b6f0be037563ee538a10f76ffe43e8b1801f5e8
Sosodope <sonadolkar93@hotmail.com>
2018-08-28 23:04:31 +0700

    add bulma and set up sass styling

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <footer>
+        <h5>Footer</h5>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
