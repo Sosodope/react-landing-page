7f1acee2aedaf520552e69c50c321c65e3b41ddf
Sosodope <sonadolkar93@hotmail.com>
2018-08-28 23:31:51 +0700

    set up header component and styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
   render() {
     return (
-      <div className="header">
-        <header>
-          <h1>My App</h1>
-        </header>
+      <div className="nav has-shadow">
+        <div className="container">
+          <div className="nav-left">
+            <a className="nav-item">GoodWeb</a>
+          </div>
+
+          <span className="nav-toggle">
+            <span />
+            <span />
+            <span />
+          </span>
+
+          <div className="nav-right nav-menu">
+            <div className="nav-item">
+              <p className="control">
+                <a className="button is-primary is-outlined">
+                  <span className="icon">
+                    <i class="fas fa-comment-alt" />
+                    <span>Contact Us</span>
+                  </span>
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
